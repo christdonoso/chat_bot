116b4677841976afa359a3e728cb23051994fd49
Christian Donoso <christ.donoso@gmail.com>
2025-01-29 15:09:48 -0300

    chuck

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash # exit on error set -o errexit 
  
 pip install -r requirements.txt 
-python manage.py makemigrations
-python manage.py migrate
+python3 manage.py makemigrations
+python3 manage.py migrate
 
 
